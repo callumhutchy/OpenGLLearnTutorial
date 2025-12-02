5025e020f47920fc6fe08e9dd505b398c244e07a
Callum Hutchinson <callumhutchie@hotmail.co.uk>
2025-12-02 23:34:48 +0000

    Buffer clearing, moving onto next chapter

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+//Place to handle user input
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -18,12 +19,13 @@ void processInput(GLFWwindow *window)
 
 int main()
 {
-
+    //Init GLFW and specify opengl version to use (3.3)
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    //Create the window context with dimensions and name
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -39,14 +41,23 @@ int main()
         return -1;
     }
 
+    //Initial viewport dimensions
     glViewport(0, 0, 800, 600);
 
+    //Register callback to adjust viewport when resized
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    //Keep window open until told to close
     while (!glfwWindowShouldClose(window))
     {
+        //Handle user input
         processInput(window);
 
+        //rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
